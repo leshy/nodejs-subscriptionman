084cabe721b5bb0fb3220c53ee3f8effd3ec39e1
lesh <lesh@sysphere.org>
2012-09-12 23:51:29 +0200

    oneshot

--- index.js
+++ index.js
@@ -8,26 +8,34 @@ exports.SubscriptionMan = backbone.Model.extend4000({
     },
 
     subscribe: function (pattern,callback,name) {
+
         if (!name) { name = Math.random() } // need something else here, callback can be reused for different patterns... 
         if (this.patterns[name]) { throw "subscription with name " + name + " already exists" }
-        this.patterns[name] = { validator: Validator(pattern), callback: callback }
+        var subscriber
+        this.patterns[name] = subscriber = { validator: Validator(pattern), callback: callback }
+        this.trigger('subscribe', subscriber)
         return name
     },
     
+    subscribers: function () {
+        return _.values(this.patterns)
+    },
+
     unsubscribe: function (name) {
         if (!this.patterns[name]) { throw "subscription with name " + name + " not found" }
         delete this.patterns[name]
     },
     
-    oneshot: function(msg,f) {
+    oneshot: function(pattern,callback,name) {
         var self = this;
-        function ff() { self.unsubscribe(ff); f.apply(this,arguments); }
-        this.subscribe(msg, ff );
-        return function() { self.unsubscribe(ff); };
+        function ff() { self.unsubscribe(name); callback.apply(this,arguments); }
+        name = this.subscribe(pattern, ff, name );
+        return function() { self.unsubscribe(name); };
     },
     
-    msg: function (msg,callback) {
+    msg: function (msg) {
         var args = _.flatten(_.map(_.values(this.patterns), function (matcher) { return [ matcher.validator, matcher.callback] }))
+        if (!args.length) { return }
         args.unshift(msg)
         Select.apply(this, args)
     }
